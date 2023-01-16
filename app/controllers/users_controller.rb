aef0323cc51060d5f472b4ba35eb0264f78f824f
disanchez27edu <disanchez@uchicago.edu>
2023-01-16 21:12:12 +0000

    finished user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
 def show
-  @user = User.find_by!(username: params.fetch(:id))
+  @author = current_user
 end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "photos#index"
+
   resources :likes
   resources :follow_requests
   resources :comments
@@ -7,6 +8,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: :show
+
+  get "/:username" => "users#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
